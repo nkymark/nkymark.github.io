264f4ee614197d02ae92b9940bc9936e1b57c6c9
nkymark <mark.ng@ulster.ac.uk>
2019-02-14 02:27:19 +0000

    Add files via upload

---
+++ About_Me_files/About_Me.js
@@ -0,0 +1,11 @@
+// Created by iWeb 3.0.4 local-build-20181212
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke()});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('About_Me_files/About_MeMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');Widget.onload();fixAllIEPNGs('Media/transparent.gif');fixupIECSS3Opacity('id4');applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
