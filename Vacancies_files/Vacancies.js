1e191b7f9a93e6d19d2e19dfc75f9ad0c1337009
nkymark <mark.ng@ulster.ac.uk>
2019-02-14 02:30:48 +0000

    Add files via upload

---
+++ Vacancies_files/Vacancies.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20181212
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Vacancies_files/VacanciesMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');Widget.onload();fixAllIEPNGs('Media/transparent.gif');fixupIECSS3Opacity('id3');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
